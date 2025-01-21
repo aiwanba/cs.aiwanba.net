c406b659eaace79447dc762d55ab78447bd13aa7
aiwanba <aiwanyule@qq.com>
2025-01-21 10:53:13 +0800

    01

--- gunicorn_conf.py
+++
@@ -1,9 +0,0 @@
-# Gunicorn配置
-bind = '0.0.0.0:5010'
-workers = 4
-threads = 2
-worker_class = 'sync'
-pidfile = 'gunicorn.pid'
-accesslog = 'logs/gunicorn_access.log'
-errorlog = 'logs/gunicorn_error.log'
-loglevel = 'info' 
--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .company import Company
-from .stock import Stock
-from .exchange import Exchange
-from .bank import Bank
-from .ai_company import AICompany 
