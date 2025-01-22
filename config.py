61fafad1bf089f27e2dbf548448c709bc62e05d2
aiwanba <aiwanyule@qq.com>
2025-01-22 12:25:17 +0800

    01

--- config.py
+++ config.py
@@ -7,10 +7,20 @@ class Config:
     
     # 数据库配置
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql://root:password@localhost:3306/stock_game'
+        'mysql://cs_aiwanba_net:sQz9HSnF5ZcXj9SX@localhost:3306/cs_aiwanba_net'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # 游戏相关配置
     INITIAL_BALANCE = 100000  # 玩家初始资金
     MIN_STOCK_PRICE = 1  # 最低股票价格
-    MAX_STOCK_PRICE = 1000  # 最高股票价格 
+    MAX_STOCK_PRICE = 1000  # 最高股票价格
+
+class DevelopmentConfig(Config):
+    """开发环境配置"""
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """生产环境配置"""
+    DEBUG = False
+    # 生产环境特定配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://cs_aiwanba_net:sQz9HSnF5ZcXj9SX@localhost:3306/cs_aiwanba_net' 
