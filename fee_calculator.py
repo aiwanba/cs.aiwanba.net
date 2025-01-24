c8ad1736160ff78f348ff8f056c3a7cd4e41a309
aiwanba <aiwanyule@qq.com>
2025-01-24 18:37:34 +0800

    01

--- fee_calculator.py
+++
@@ -1,3 +0,0 @@
-def calculate_fee(amount: float) -> float:
-    """计算交易手续费（费率0.3%）"""
-    return round(amount * 0.003, 2) 
--- static/js/main.js
+++
@@ -1,4 +0,0 @@
-// 自定义JavaScript
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('页面加载完成');
-}); 
