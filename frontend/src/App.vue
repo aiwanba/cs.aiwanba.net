d3b704f276966d0d7da03df6363528fee857c845
aiwanba <aiwanyule@qq.com>
2025-01-27 19:58:38 +0800

    序号: 012

---
+++ frontend/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-container class="app-container">
+    <el-header>
+      <nav-bar></nav-bar>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <side-menu></side-menu>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+import SideMenu from './components/SideMenu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    SideMenu
+  }
+}
+</script>
+
+<style>
+.app-container {
+  height: 100vh;
+}
+
+.el-header {
+  background-color: #409EFF;
+  color: white;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #f8f9fa;
+}
+</style> 
