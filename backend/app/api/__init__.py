0569f3fc6498807724691451b318d58f5103fa44
aiwanba <aiwanyule@qq.com>
2025-02-01 08:36:56 +0800

    01

--- backend/app/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from .auth import auth_bp
-from .company import company_bp
-from .bank import bank_bp
-from .trade import trade_bp
-from .message import message_bp
-
-__all__ = ['auth_bp', 'company_bp', 'bank_bp', 'trade_bp', 'message_bp'] 
